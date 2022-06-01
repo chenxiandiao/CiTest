5b02541fcc34ca7bce8fa8fae8e8f4e5fffece92
chenxiandiao@vastchain.ltd <Cxd316213!>
2022-06-01 11:10:55 +0800

    feat: init

---
+++ app/src/main/java/ltd/vastchain/ci/MainActivity.kt
@@ -0,0 +1,11 @@
+package ltd.vastchain.ci
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
